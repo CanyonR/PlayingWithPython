73aeb84d513a7424f73b5d6e6a9154d560285ed4
Canyon Read <canyonread@gmail.com>
2022-04-21 21:31:02 -0700

    ifnamemain

--- SimpleSpinner.py
+++ SimpleSpinner.py
@@ -1,12 +1,16 @@
 import time
 
-print("Look what I can do: \033[1A")
-spinnerlist = ["-", "\\", "|", "/"]
+def main():
+    print("Look what I can do: \033[1A")
+    spinnerlist = ["-", "\\", "|", "/"]
 
-loopCount = 0
-while loopCount < 3:
-    for icon in range(len(spinnerlist)):
-        print('\033[20C' + spinnerlist[icon], end="\r")
-        time.sleep(0.3333333)
-    loopCount += 1
-print("\033[K" + "You snooze you lose!")
+    loopCount = 0
+    while loopCount < 3:
+        for icon in range(len(spinnerlist)):
+            print('\033[20C' + spinnerlist[icon], end="\r")
+            time.sleep(0.3333333)
+        loopCount += 1
+    print("\033[K" + "You snooze you lose!")
+
+if __name__ == '__main__':
+    main()
