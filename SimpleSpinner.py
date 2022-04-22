8464bd2229ad3a4b569261f40117f2f41603aff1
Canyon Read <canyonread@gmail.com>
2022-04-21 21:55:32 -0700

    ifnamemain

--- SimpleSpinner.py
+++ SimpleSpinner.py
@@ -2,13 +2,13 @@ import time
 
 
 def main():
-    print("Look what I can do: \033[1A")
+    print("  Look what I can do: \033[1A")
     spinnerlist = ["-", "\\", "|", "/"]
 
     loopCount = 0
     while loopCount < 3:
         for icon in enumerate(spinnerlist):
-            print('\033[20C' + spinnerlist[icon[0]], end="\r")
+            print('\033[22C' + spinnerlist[icon[0]], end="\r")
             time.sleep(0.3333333)
         loopCount += 1
     print("\033[K" + "You snooze you lose!")
